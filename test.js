e793ed99ae139f4ae3263dfa0586bce1f7c55229
Keith Cirkel <keithamus@users.noreply.github.com>
2016-11-04 09:58:09 +0000

    feat: add test config, which disables all flowtyping

---
+++ test.js
@@ -0,0 +1,7 @@
+'use strict';
+config = require('./flowtype');
+config.settings.flowtype.onlyFilesWithFlowAnnotation = false;
+Object.keys(config.rules).forEach(function (rule) {
+  config.rules[rule] = 0;
+});
+module.exports = config;
